1f335d0ee10c0a95908438bffce2cb050e252849
Jinal <Jinal@JSN>
2016-08-17 22:50:41 -0400

    Add project files.

---
+++ mvcsample/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(mvcsample.Startup))]
+namespace mvcsample
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
